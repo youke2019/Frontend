a65be04059d54162860006661d497f7c363f4485
zhaoyang110157 <1853935753@qq.com>
2019-07-04 11:34:49 +0800

    update

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ *
+ @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,TouchableOpacity} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import User from './src/pages/User';
+import Main from './src/pages/Main'
+import Navbar from './src/component/Navbar'
+
+
+const RootStack = createStackNavigator({
+  Main : Main,
+  User : User
+},{
+  initialRouteName : 'Main'
+})
+export default class App extends Component {
+  render() {
+    return (
+    <View>
+      <RootStack/>
+      <Navbar/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
---
+++ src/component/Navbar.js
@@ -0,0 +1,68 @@
+
+import React, {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    PixelRatio
+    } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import User from '../pages/User';
+import Main from '../pages/Main';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+        <View style={styles.navbar}>
+          <TouchableOpacity
+              style={styles.button}
+               onPress ={()=>{this.props.navigation.navigate('Main')}}
+          >
+            <Text style = {styles.instructions}>主页</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.button}
+              onPress ={()=>{alert("课程库")}}
+          >
+            <Text style = {styles.instructions}>课程库</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.button}
+              onPress ={()=>{alert("课程表")}}
+          >
+            <Text style = {styles.instructions}>课程表</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.button}
+              onPress ={()=>{alert("精彩瞬间")}}
+          >
+            <Text style = {styles.instructions}>精彩瞬间</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.button}
+              onPress ={()=>{this.props.navigation.navigate('User')}}
+          >
+            <Text style = {styles.instructions}>我的</Text>
+          </TouchableOpacity>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  navbar:{
+    position:'absolute',
+    bottom:0,
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    height:40
+    },
+  button:{
+    height:40,
+    width:70,
+    borderRadius:20,
+    overflow:'hidden'
+  }
+});
